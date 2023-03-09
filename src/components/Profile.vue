7358ed93e6e9371197e21d911c9b93c13db72c5c
Ferrari-coder <oluwaferanmiaiyegbusi@gmail.com>
2023-03-09 22:34:52 +0100

    profile section completed

---
+++ src/components/Profile.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="container">
+        <div class="image"><img src="../assets/avi.jpg" alt="avi"></div>
+        <div class="profile-content">
+            <h2>Aiyegbusi Oluwaferanmi</h2>
+            <p>@Ferrari-coder</p>
+            <h5>Front-End Engineer</h5>
+            <section class="section">
+                <div class="stat" id="repos">
+                    <p class="profile-figures" id="repo-fig">21</p>
+                    <h3 class="profile-details"> Repositories</h3>
+                </div>
+                <div class="stat">
+                    <p class="profile-figures">10</p>
+                    <h3 class="profile-details"> Followers</h3>
+                </div>
+                <div class="stat">
+                    <p class="profile-figures">22</p>
+                    <h3 class="profile-details">Following</h3>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfileSection',
+}
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:math';
+
+/* variables*/
+$primary: #4D73F8;
+$base-gap: 30px;
+$text-color: #020C18;
+$background: #ffffff;
+
+.container {
+    display: flex;
+    gap: $base-gap;
+    align-items: center;
+    background: rgba(77, 115, 248, 0.03);
+    border: 1px solid rgba(77, 115, 248, 0.15);
+    padding: $base-gap;
+    border-radius: $base-gap/3;
+
+
+    .image {
+        width: 222px;
+        height: 222px;
+        padding: 5px;
+        position: relative;
+        border-radius: 50%;
+        background: linear-gradient(to bottom, rgba(77, 115, 248, 1), rgba(77, 115, 248, 0.15));
+
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+        }
+    }
+
+    .profile-content {
+        line-height: 15px;
+
+        h2 {
+            font-family: 'Merriweather';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 30px;
+            color: $text-color;
+        }
+
+        p {
+            font-weight: 400;
+            font-size: 16px;
+            color: #616161;
+            font-family: 'Roboto';
+            line-height: 10px;
+        }
+
+        h5 {
+            font-weight: 600;
+            font-size: 20px;
+            color: $text-color;
+            line-height: 10px;
+        }
+
+        .section {
+            display: flex;
+            gap: $base-gap;
+
+            .stat {
+                padding: $base-gap/3;
+                width: 150px;
+                background: rgba(77, 115, 248, 0.2);
+                border-radius: 4px;
+                text-align: center;
+                line-height: $base-gap/3;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/Repos.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="container">
+    <h1 className="repo-header">Repositories</h1>
+    <div class="repo">
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+      <div class="repo-container">
+        <h2>Carousel-review</h2>
+        <p class="desc">Carousel-review react js</p>
+        <p class="id">ID:87979997</p>
+        <div class="lang-size">
+          <p className="repo-lang">React js</p>
+          <p className="repo-size">140Kb</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RepoSection',
+}
+</script>
+
+<style lang="scss" scoped>
+
+@use 'sass:math';
+
+/* variables*/
+$primary: #4D73F8;
+$base-gap: 30px;
+$text-color: #020C18;
+$background: #ffffff;
+
+.container {
+  margin: 30px 0;
+
+  .repo-header {
+    font-family: 'Merriweather';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    color: $text-color;
+  }
+
+  .repo{
+    padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  }
+}
+</style>
